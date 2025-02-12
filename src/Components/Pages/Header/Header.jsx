56b60d3506cf369658b88ac61525b7f6d7ea79c6
Janderson Oliveira Pacheco <janderson.katsu@gmail.com>
2025-02-12 12:41:32 -0300

    Alterado botão de pesquisa para cancelar uma busca.

--- src/Components/Pages/Header/Header.jsx
+++ src/Components/Pages/Header/Header.jsx
@@ -9,12 +9,20 @@ const Header = () => {
         setSearch(event.target.value)
     }
 
+    function cancelSearch(event){
+        event.preventDefault()
+
+        setSearch('')
+    }
+    
+
     return (
         <>
             <nav className={styles.nav}>
                 <form className={styles.selectForm}>
                     <input type="text" id="search" autoComplete="off"placeholder="Procure por uma série" value={search} onChange={handleSearch}></input>
-                    <button id="searchBtn" disabled>Pesquisar</button>
+
+                    <button id="searchBtn" disabled={search.trim() === ''} onClick={cancelSearch}>Cancelar</button>
                 </form>
             </nav> 
         </>
