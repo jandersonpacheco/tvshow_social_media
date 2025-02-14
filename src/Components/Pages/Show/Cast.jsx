b6b0d4912e7361f7c28c54a00536503e2b552ec5
Janderson Oliveira Pacheco <janderson.katsu@gmail.com>
2025-02-14 11:57:47 -0300

    Fotos do elenco estilizadas.

--- src/Components/Pages/Show/Cast.jsx
+++ src/Components/Pages/Show/Cast.jsx
@@ -5,11 +5,16 @@ const Cast = ({cast}) => {
     return (
         <div className={styles.castContainer}>
             <div className={styles.castContent}>
-                {cast.profile_path && (
+                {cast.profile_path ? (
                     <img
                         src={`https://image.tmdb.org/t/p/w500${cast.profile_path}`}
                         className={styles.castImg}
                     />
+                ):(
+                    <img
+                    src={`https://i2.wp.com/static.vecteezy.com/system/resources/previews/001/840/618/original/picture-profile-icon-male-icon-human-or-people-sign-and-symbol-free-vector.jpg`}
+                        className={styles.castImg}
+                />
                 )}
                 <h3 className={styles.castName}>{cast.name}</h3>
                 <h3 className={styles.castCharacter}>{cast.roles[0].character}</h3>
