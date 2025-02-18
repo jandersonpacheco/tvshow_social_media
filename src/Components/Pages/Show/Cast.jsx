6be7aeed32371c66b1804da323f9f93480ae601a
Janderson Oliveira <janderson.katsu@gmail.com>
2025-02-17 23:37:27 -0300

    Ajustado css do showDetails, onde o castImg ultrapassava o limite da tela gerando um scrool-X

--- src/Components/Pages/Show/Cast.jsx
+++ src/Components/Pages/Show/Cast.jsx
@@ -5,11 +5,16 @@ const Cast = ({cast}) => {
     return (
         <>
             <div className={styles.castContent}>
-                {cast.profile_path && (
+                {cast.profile_path ? (
                     <img
                         src={`https://image.tmdb.org/t/p/w500${cast.profile_path}`}
                         className={styles.castImg}
                     />
+                ):(
+                    <img
+                    src={`https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT-pQbtqNJoZrYcg0-7A6as6IDzHfdJrFEv9A&s`}
+                    className={styles.castImg}
+                />
                 )}
                 <h3 className={styles.castName}>{cast.name}</h3>
                 <h3 className={styles.castCharacter}>{cast.roles[0].character}</h3>
