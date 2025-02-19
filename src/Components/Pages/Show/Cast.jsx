09f355e79dc812a97686d985ab4c6c23a095b139
Janderson Oliveira Pacheco <janderson.katsu@gmail.com>
2025-02-19 08:53:12 -0300

    commit pre pull

--- src/Components/Pages/Show/Cast.jsx
+++ src/Components/Pages/Show/Cast.jsx
@@ -12,13 +12,8 @@ const Cast = ({cast}) => {
                     />
                 ):(
                     <img
-<<<<<<< HEAD
-                    src={`https://i2.wp.com/static.vecteezy.com/system/resources/previews/001/840/618/original/picture-profile-icon-male-icon-human-or-people-sign-and-symbol-free-vector.jpg`}
-                        className={styles.castImg}
-=======
                     src={`https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT-pQbtqNJoZrYcg0-7A6as6IDzHfdJrFEv9A&s`}
                     className={styles.castImg}
->>>>>>> 132de86b8094ca0ae80b3ed1abdafcb6886bceee
                 />
                 )}
                 <h3 className={styles.castName}>{cast.name}</h3>
