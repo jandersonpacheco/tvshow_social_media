f7c0501ed0dc327defb3a4f8bbdb22779c36a106
Janderson Oliveira <janderson.katsu@gmail.com>
2025-05-16 00:27:23 -0300

    Estilizado icone de lo usuário ao logar no site.

--- src/Components/Pages/Header/Header.jsx
+++ src/Components/Pages/Header/Header.jsx
@@ -24,19 +24,18 @@ const Header = () => {
             {/*<Link to={`/home`} onClick= {() => homeBtn()}>
                 <h1>Home</h1>
             </Link>*/}
-                    {profile !== "" && (
-                        <div>
-                            <img src={profile.picture} alt="imagem do usuário"/>
-                            <h3>Usuário logado</h3>
-                            <p>Nome: {profile.name}</p>
-                            <p>Email: {profile.email}</p>
-                            <button className={styles.button} onClick={() => logout({/*navigate*/})}>Sair</button>
-                        </div>
-                    )}
                 <form className={styles.selectForm}>
-                    <input type="text" id="search" autoComplete="off"placeholder="Procure por uma série" value={search} onChange={handleSearch}></input>
-                    <button id="searchBtn" disabled={search.trim() === ''} onClick={cancelSearch}>Cancelar</button>
+                    <input className={styles.searchBar} type="text" id="search" autoComplete="off"placeholder="Procure por uma série" value={search} onChange={handleSearch}></input>
+                    <button className={styles.cancelBtn} id="searchBtn" disabled={search.trim() === ''} onClick={cancelSearch}>Cancelar</button>
                 </form>
+                {profile !== "" && (
+                    <div className={styles.userContainer}>
+                        <img src={profile.picture} alt="imagem do usuário" className={styles.userPhoto}/>
+                        {/*<p>Nome: {profile.name}</p>
+                        <p>Email: {profile.email}</p>*/}
+                        <button className={styles.logoutBtn} onClick={() => logout({/*navigate*/})}>Sair</button>
+                    </div>
+                )}
             </nav> 
         </>
     )
