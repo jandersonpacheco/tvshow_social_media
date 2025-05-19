8dde3bb8cbace482e7ad79d07bb227db158a5392
Janderson Oliveira Pacheco <janderson.katsu@gmail.com>
2025-05-19 13:32:09 -0300

    Criado botão de home e login e adicionada rotas para lidar com os botões.

--- src/Components/Pages/Header/Header.jsx
+++ src/Components/Pages/Header/Header.jsx
@@ -2,12 +2,22 @@ import styles from "./style.module.css"
 import useTvShowStore from "../../../store/tvShowStore.js"
 import { useNavigate } from "react-router-dom"
 import SSOUserInfo from '../../../store/SSOUserInfo.js'
+import homeIcon from '../../../assets/home.png' // Alterar para não importar a imagem diretamente
 
 const Header = () => {
     const {search, setSearch} = useTvShowStore()
     const { profile } = SSOUserInfo()
     const { logout } = SSOUserInfo()
     const navigate = useNavigate('/login')
+
+    const handleLogout = () => {
+        const confirm = window.confirm(`Deseja realmente sair da sua conta, ${profile.name}?`)
+
+        if(confirm){
+            logout()
+            return navigate('/')
+        }
+    }
 
     const handleSearch = (event) => setSearch(event.target.value)
 
@@ -21,29 +31,23 @@ const Header = () => {
     return (
         <>
             <nav className={styles.nav}>
-            {/*<Link to={`/home`} onClick= {() => homeBtn()}>
-                <h1>Home</h1>
-            </Link>*/}
-                    {profile !== "" && (
-                        <div>
-                            <img src={profile.picture} alt="imagem do usuário"/>
-                            <h3>Usuário logado</h3>
-                            <p>Nome: {profile.name}</p>
-                            <p>Email: {profile.email}</p>
-                            <button className={styles.button} onClick={() => logout({navigate})}>Sair</button>
-                        </div>
-                    )}
+                <h1 className={styles.homeIcon} onClick={() => navigate('/')}><img src={homeIcon} alt="Home"></img></h1>
+
                 <form className={styles.selectForm}>
                     <input className={styles.searchBar} type="text" id="search" autoComplete="off"placeholder="Procure por uma série" value={search} onChange={handleSearch}></input>
                     <button className={styles.cancelBtn} id="searchBtn" disabled={search.trim() === ''} onClick={cancelSearch}>Cancelar</button>
                 </form>
-                {profile !== "" && (
+                {profile !== "" ? (
                     <div className={styles.userContainer}>
                         <img src={profile.picture} alt="imagem do usuário" className={styles.userPhoto}/>
                         {/*<p>Nome: {profile.name}</p>
                         <p>Email: {profile.email}</p>*/}
-                        <button className={styles.logoutBtn} onClick={() => logout({/*navigate*/})}>Sair</button>
+                        <button 
+                        className={styles.logoutBtn}
+                        onClick={() => handleLogout()}>Sair</button>
                     </div>
+                ):(
+                    <button className={styles.logoutBtn} onClick={() => navigate('/login')}>Entrar / Cadastre-se</button>
                 )}
             </nav> 
         </>
